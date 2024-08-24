5e370cdccff110b95cffef98b328f160978a7089
PrettyDuna <127952072+JAJUa@users.noreply.github.com>
2024-08-24 17:14:11 +0900

    카운트다운

---
+++ Assets/CountDown.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] CameraSetting camSet;
+
+    // 초기 세팅
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        camSet = GameObject.Find("Camera Setting").GetComponent<CameraSetting>();
+    }
+
+    // 임시로 넣은 거 
+    void Start()
+    {
+        CountStart(); //Test
+    }
+
+    // 카운트 시작
+    public void CountStart()
+    {
+        anim.SetTrigger("countDown");
+    }
+
+    // 카운트 끝날 시
+    public void CountEnd()
+    {
+        camSet.CameraAnimation();
+    }
+}
---
+++ Assets/Script/CameraSetting.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+using VInspector;
+
+public class CameraSetting : MonoBehaviour
+{
+    [Tab("Settings")]
+    [ReadOnly] [SerializeField] CinemachineVirtualCamera virtualCamera;
+    [ReadOnly] [SerializeField] Transform player;
+
+    /*[Tab("Modify")]
+    [Range(1, 10)] [SerializeField] float startSize = 5.5f;
+    [Range(1, 10)] [SerializeField] float endSize = 5;
+    [Range(0, 5)] [SerializeField] float duration = 1;*/
+
+    // 초기 세팅
+    private void Awake()
+    {
+        virtualCamera = GameObject.Find("VCam").GetComponent<CinemachineVirtualCamera>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        //virtualCamera.m_Lens.OrthographicSize = startSize;
+    }
+
+    // 애니메이션 실행
+    public void CameraAnimation() 
+    {
+        virtualCamera.Follow = player;
+
+        /*DOTween.To(() => virtualCamera.m_Lens.OrthographicSize,
+                   x => virtualCamera.m_Lens.OrthographicSize = x, endSize, duration);*/
+    }
+}
